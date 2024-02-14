746966faaa39aa6a4b679e453af1f8f73dfaa251
Kaushik Lad <55614425+Kaushikladnk@users.noreply.github.com>
2024-02-14 18:26:50 +0530

    Bacic vaiable and Datatypes

---
+++ 01_var.js
@@ -0,0 +1,16 @@
+/* ---------------------- Variables in JS ------------------------------------ */
+
+const acId = 3232                   /* Never Change Value of Const*/
+let accEmail = "kashk@gmail.com"    /* Always Use let as a Veriable  */
+var accPass = "kauhsik5252"         /* never Use var as a variabale because issue in block scope and function scope  { This Is Scope } */  
+accCity = "Surat"
+
+let accuntstate;          /* if you not assigne Any value in variable javascript use as undefined  */
+
+accEmail = "hello@gmail.com"
+accPass = "Jignal@65"
+accCity = "Nvss"
+
+console.log(acId);   /* Print value in JS */
+
+console.table ([accEmail, accPass, accCity, accuntstate ])   /*  Print value in table formate in js */
---
+++ 02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // trat all JS code as new version 
+
+// alert("Hello") // we are using nodejs, not browser
+
+let name = "kauhsik" 
+let age = 10
+let isLoggin = false
+let city = null
+
+// Numeric 
+
+// Number =>2 to power 53 
+// bigint EX - trading stock market and big website
+
+// Primetive
+
+// string => ""
+// boolean => true/false
+// null => standalone value   
+// undefine  => value not Assigned 
+// symbol  => unique 
+
+console.log(typeof null);   // Object
+console.log(typeof undefined);  // undefined
+console.log(typeof age);
+
+
+
+
+
+
+
+
+
+
